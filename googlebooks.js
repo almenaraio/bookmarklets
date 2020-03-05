e4c5a9cb339efbd581fda65be517c51ea6d55eec
almenaracarlos <32857704+almenaraio@users.noreply.github.com>
2020-03-05 16:23:41 -0500

    Delete googlebooks.js

--- googlebooks.js
+++
@@ -1,1 +0,0 @@
-javascript:Qr=window.getSelection?window.getSelection().toString():document.selection.createRange().text;if(!Qr){void(Qr=prompt('Enter ISBN-10, e.g. 1439876800:',''))}if(Qr)location.href='http://books.google.com/books?isbn='+escape(Qr)

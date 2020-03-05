803bd7af833dd9122a58f8007ec92ad987e4a553
almenaracarlos <32857704+almenaraio@users.noreply.github.com>
2020-03-05 16:23:31 -0500

    Delete amazon_tab.js

--- amazon_tab.js
+++
@@ -1,1 +0,0 @@
-javascript:Qr=window.getSelection?window.getSelection().toString():document.selection.createRange().text;if(!Qr){void(Qr=prompt('Enter ISBN-10 or ASIN to resolve, e.g. 1439876800:',''))}if(Qr)window.open(location.href='http://www.amazon.com/dp/'+escape(Qr))(window.location);
